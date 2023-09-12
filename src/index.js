9c1a2faf04ed44dfff388af8797bb1189abe6342
Bryan Chow <bchow.post@gmail.com>
2023-09-12 20:31:51 +0100

    Gameboard functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,82 @@
 const shipFactory = (len) => {
-	let hits = 0;
-	const hit = () => {
-		hits += 1;
-		return hits;
+	const hits = 0;
+	return {
+		len,
+		hit() {
+			this.hits += 1;
+		},
+		isSunk() {
+			return this.hits >= len;
+		},
+		hits,
 	};
-	const isSunk = () => hits >= len;
-	return { len, hit, isSunk, hits };
 };
 
-export { shipFactory };
+const gameBoardFactory = () => {
+	const ships = [];
+	const missedAttacks = [];
+
+	const placeShip = (x, y, len, orientation) => {
+		const tempShip = shipFactory(len);
+		tempShip.coordinates = [];
+		if (orientation === "X" && x > 10 - len) {
+			return ships;
+		}
+		if (orientation === "Y" && y < len - 1) {
+			return ships;
+		}
+		if (orientation === "X") {
+			for (let i = 0; i < len; i += 1) {
+				tempShip.coordinates.push(`${[x + i, y]}`);
+			}
+			const contains = ships.some((ship) =>
+				ship.coordinates.some((coordinate) =>
+					tempShip.coordinates.includes(coordinate),
+				),
+			);
+			if (contains) {
+				return ships;
+			}
+			ships.push(tempShip);
+			return ships;
+		}
+		if (orientation === "Y") {
+			for (let i = 0; i < len; i += 1) {
+				tempShip.coordinates.push(`${[x, y - i]}`);
+			}
+			const contains = ships.some((ship) =>
+				ship.coordinates.some((coordinate) =>
+					tempShip.coordinates.includes(coordinate),
+				),
+			);
+			if (contains) {
+				return ships;
+			}
+			ships.push(tempShip);
+			return ships;
+		}
+		return ships;
+	};
+
+	const receiveAttack = (x, y) => {
+		const coord = `${[x, y]}`;
+		if (missedAttacks.includes(coord)) {
+			return false;
+		}
+		ships.forEach((item) => {
+			if (item.coordinates.includes(coord)) {
+				item.hit();
+			} else {
+				missedAttacks.push(coord);
+			}
+		});
+	};
+
+	const isAllSunk = () => {
+		const sunk = ships.every((ship) => ship.isSunk() === true);
+		return !!sunk;
+	};
+	return { placeShip, receiveAttack, ships, missedAttacks, isAllSunk };
+};
+
+export { shipFactory, gameBoardFactory };
--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,120 @@
 /* eslint-disable no-undef */
-import { shipFactory } from ".";
+import { shipFactory, gameBoardFactory } from ".";
 
-const destroyer = shipFactory(4);
+describe("shipFactory", () => {
+	const destroyer = shipFactory(4);
+	test("shipFactory len", () => {
+		expect(destroyer.len).toBe(4);
+	});
 
-test("shipFactory len", () => {
-	expect(destroyer.len).toBe(4);
+	test("shipFactory sunk", () => {
+		destroyer.hit();
+		destroyer.hit();
+		destroyer.hit();
+		destroyer.hit();
+		expect(destroyer.isSunk).toBeTruthy();
+	});
+
+	test("shipFactory hits", () => {
+		destroyer.hit();
+		destroyer.hit();
+		expect(destroyer.hits).toBe(6);
+	});
 });
 
-test("shipFactory hit", () => {
-	destroyer.hit();
-	destroyer.hit();
-	destroyer.hit();
-	destroyer.hit();
-	expect(destroyer.isSunk).toBeTruthy();
-});
+describe("gameBoard", () => {
+	test("gameBoard coordinates X direction", () => {
+		expect(
+			gameBoardFactory().placeShip(2, 0, 2, "X")[0].coordinates,
+		).toEqual(["2,0", "3,0"]);
+	});
 
-test("gameBoard coordinates");
+	test("gameBoard coordinates X direction fail", () => {
+		expect(gameBoardFactory().placeShip(6, 0, 5, "X")[0]).toBeUndefined();
+	});
+
+	test("gameBoard coordinates Y direction", () => {
+		expect(
+			gameBoardFactory().placeShip(0, 3, 4, "Y")[0].coordinates,
+		).toEqual(["0,3", "0,2", "0,1", "0,0"]);
+	});
+
+	test("gameBoard coordinates Y direction fail", () => {
+		expect(gameBoardFactory().placeShip(0, 3, 5, "Y")[0]).toBeUndefined();
+	});
+
+	test("gameBoard place duplicate ships X", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 1, 2, "Y");
+		g.placeShip(0, 0, 2, "X");
+		expect(g.ships.length).toBe(1);
+	});
+
+	test("gameBoard place duplicate ships Y", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 5, 5, "Y");
+		g.placeShip(0, 1, 2, "Y");
+		g.placeShip(0, 6, 4, "Y");
+		expect(g.ships.length).toBe(1);
+	});
+
+	test("gameBoard receive attack", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 0, 2, "X");
+		g.receiveAttack(0, 0);
+		expect(g.ships[0].hits).toBe(1);
+	});
+
+	test("gameBoard receive attack 2", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 0, 2, "X");
+		g.receiveAttack(0, 0);
+		g.receiveAttack(1, 0);
+		expect(g.ships[0].isSunk()).toBeTruthy();
+	});
+
+	test("gameBoard receive attack X", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 0, 4, "X");
+		g.placeShip(3, 3, 5, "X");
+		g.receiveAttack(3, 3);
+		g.receiveAttack(4, 3);
+		g.receiveAttack(5, 3);
+		g.receiveAttack(6, 3);
+		g.receiveAttack(7, 3);
+		expect(g.ships[1].isSunk()).toBeTruthy();
+	});
+
+	test("gameBoard receive attack Y", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 3, 2, "Y");
+		g.receiveAttack(0, 3);
+		g.receiveAttack(0, 2);
+		expect(g.ships[0].isSunk()).toBeTruthy();
+	});
+
+	test("gameBoard receive attack miss", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 2, 2, "Y");
+		g.receiveAttack(0, 3);
+		g.receiveAttack(0, 3);
+		expect(g.missedAttacks.length).toBe(1);
+	});
+
+	test("gameBoard isAllSunk False", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 0, 1, "X");
+		g.placeShip(9, 9, 1, "Y");
+		expect(g.isAllSunk()).toBeFalsy();
+	});
+
+	test("gameBoard isAllSunk True", () => {
+		const g = gameBoardFactory();
+		g.placeShip(0, 0, 1, "X");
+		g.placeShip(9, 9, 2, "Y");
+		g.receiveAttack(0, 0);
+		g.receiveAttack(9, 9);
+		g.receiveAttack(9, 8);
+		expect(g.isAllSunk()).toBeTruthy();
+	});
+});
