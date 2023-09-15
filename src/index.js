73f7818f1734cbb3c21ddf92a5511d97939897a8
Bryan Chow <bchow.post@gmail.com>
2023-09-15 19:02:19 +0100

    Event Listeners for Attacks and Removing Attacks

--- src/index.js
+++ src/index.js
@@ -7,47 +7,76 @@ const generateGameBoard = (gameBoard) => {
 		for (let j = 0; j < 10; j++) {
 			const coordinates = `${[j, i]}`;
 			const cell = document.createElement("div");
-			cell.setAttribute("class", `cell ${gameBoard}`);
+			cell.setAttribute("class", `cell ${gameBoard}-cell`);
 			cell.setAttribute("data-key", coordinates);
 			g.appendChild(cell);
 		}
 	}
 };
 
-const displayShips = (g) => {
+generateGameBoard("g1");
+generateGameBoard("g2");
+
+const displayShips = (g, gDom) => {
+	const gameBoardDom = document.querySelector(`.${gDom}`);
 	g.ships.forEach((ship) => {
 		ship.coordinates.forEach((coordinate) => {
-			const shipCell = document.querySelector(
+			const shipCell = gameBoardDom.querySelector(
 				`[data-key="${coordinate}"]`,
 			);
-			shipCell.classList.toggle("ship");
+			shipCell.classList.add("ship");
 		});
 	});
 	g.missedAttacks.forEach((coordinate) => {
-		const missedCell = document.querySelector(`[data-key="${coordinate}"]`);
-		missedCell.classList.toggle("miss");
+		const missedCell = gameBoardDom.querySelector(
+			`[data-key="${coordinate}"]`,
+		);
+		missedCell.classList.add("miss");
 	});
 	g.hitAttacks.forEach((coordinate) => {
-		const missedCell = document.querySelector(`[data-key="${coordinate}"]`);
-		missedCell.classList.toggle("hit");
+		const missedCell = gameBoardDom.querySelector(
+			`[data-key="${coordinate}"]`,
+		);
+		missedCell.classList.add("hit");
 	});
 };
 
-generateGameBoard("gameboard1");
-generateGameBoard("gameboard2");
+const eventListenersFactory = (g, gDom) => {
+	const gameBoardDom = document.querySelector(`.${gDom}`);
+	const cellNodeList = gameBoardDom.childNodes;
+	const returnClicks = (e) => {
+		const coordinateX = e.target.dataset.key.split(",")[0];
+		const coordinateY = e.target.dataset.key.split(",")[1];
+		g.receiveAttack(coordinateX, coordinateY);
+		displayShips(g, gDom);
+	};
+	const addAttackListener = () => {
+		cellNodeList.forEach((node) => {
+			node.addEventListener("click", returnClicks);
+		});
+	};
+	const removeAttackListener = () => {
+		cellNodeList.forEach((node) => {
+			node.removeEventListener("click", returnClicks);
+		});
+	};
+	return { addAttackListener, removeAttackListener };
+};
 
 const gameLoop = () => {
 	const g1 = gameBoardFactory();
 	const g2 = gameBoardFactory();
 	const p = player("bryan", "nimitz", g1, g2);
-	g1.placeShip(0, 0, 5, "X");
-	g1.placeShip(0, 6, 4, "Y");
-	g1.receiveAttack(9, 9);
-	g1.receiveAttack(0, 0);
-	g1.receiveAttack(0, 1);
-	g1.receiveAttack();
-	console.log(g1.missedAttacks);
-	displayShips(g1);
+	const e1 = eventListenersFactory(g1, "g1");
+	const e2 = eventListenersFactory(g2, "g2");
+	g1.placeShip(0, 0, 4, "X");
+	g2.placeShip(0, 0, 6, "X");
+	displayShips(g1, "g1");
+	displayShips(g2, "g2");
+	// displayShips(g2, "g2");
+	// while (!g1.isAllSunk && !g2.isAllSunk) {
+	// 	if (p.currentBoard === g2)
+	// }
 };
 
 gameLoop();
